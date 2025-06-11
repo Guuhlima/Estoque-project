fc87b567c94397149587bdc86b184db95b56785b
Gustavo Alves Fonseca de Lima <Gustavo.Lima@wocc.com.br>
2025-06-11 11:12:08 -0300

    Adicionando o SWAGGER de Historico

--- Api-Estoque/server.js
+++ Api-Estoque/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import publicItem from './routes/publicEquipamentos.js'
 import publicUser from './routes/user.js'
+import publicHistory from './routes/publicHistory.js'
 import setupSwagger from './swagger.js' 
 
 dotenv.config();
@@ -14,5 +15,8 @@ app.use(express.json());
 app.use(cors());
 setupSwagger(app);
 app.listen(PORT, () => {`O servidor está rodando ${PORT}`})
+
+//ROTAS
 app.use('/', publicItem);
 app.use('/user', publicUser);
+app.use('/history', publicHistory)
